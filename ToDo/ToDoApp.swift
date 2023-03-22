639bc1fb5149cddf93a0278867b682813212c82a
Damien Gautier <82036710+Calligramien@users.noreply.github.com>
2023-03-22 16:35:16 +0100

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Damien Gautier on 22/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
